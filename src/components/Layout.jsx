7040de78c79c8b29c8a7472f3d6b7e3c448835c0
Pari2001 <106901290+Pari2001@users.noreply.github.com>
2023-04-21 15:23:27 +0530

    word-break

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -234,8 +234,8 @@ const NewBalance = () => {
 
                                 <div className="flexy second__color p2">
                                     <div>
-                                        <h2 className="section__title bigger__text word-break">{notebookName}</h2>
-                                        <span className="section__subtitle less__margin__subtitle word-break">{notebook[0].description.charAt(0).toUpperCase() + notebook[0].description.slice(1)}</span>
+                                        <h2 className="section__title bigger__text wordBreak">{notebookName}</h2>
+                                        <span className="section__subtitle less__margin__subtitle wordBreak">{notebook[0].description.charAt(0).toUpperCase() + notebook[0].description.slice(1)}</span>
                                     </div>
                                 </div>
 
